4f67209ebaf1f975f64bb1f09b14404abdf2776e
Darren Tsang <darren9586@gmail.com>
2022-12-28 19:20:27 +0800

    added generateJWT method

--- auth/jwtHelper.js
+++ auth/jwtHelper.js
@@ -1,10 +1,20 @@
+const jose = require('jose')
+
 const Secret = new TextEncoder().encode(
     'Hello world!',
   )
 
-function generateJWT() {
-
-
+async function generateJWT() {
+  const jwt = await new jose.SignJWT({ 'urn:example:claim': true })
+  .setProtectedHeader({ alg: 'HS256' })
+  .setIssuedAt()
+  .setIssuer('urn:example:issuer')
+  .setAudience('urn:example:audience')
+  .setExpirationTime('2h')
+  .sign(Secret)
+  
+  console.log(jwt)
 }
 
+//generateJWT()
 module.exports = {Secret, generateJWT}
