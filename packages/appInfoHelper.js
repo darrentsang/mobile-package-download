6a7bf2a6d7edeeaf39a61b22dbf43f59be67dc85
Darren Tsang <darren9586@gmail.com>
2022-12-29 18:49:54 +0800

    print app Info

--- packages/appInfoHelper.js
+++ packages/appInfoHelper.js
@@ -4,14 +4,12 @@ const AppInfoParser = require('app-info-parser')
 function extractAppInfo(filePath) {
     const parser = new AppInfoParser(filePath);
     parser.parse().then(result => {
-        console.log('app info ----> ', result)
+        console.log('app info ----> ', JSON.stringify(result))
         console.log('app info lenght ----> ', JSON.stringify(result).length)
-        //console.log('icon base64 ----> ', result.icon)
     }).catch(err => {
         console.log('err ----> ', err)
     })
 }
 
 //extractAppInfo('./data/packages/bitbar-ios-sample.ipa')
-extractAppInfo('./data/packages/bitbar-sample-app.apk')
 //extractAppInfo('./data/packages/calculator.ipa')
