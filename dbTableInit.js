d546a4755148c3c189edfe5a4ac177f08caa06b9
Darren Tsang <darren9586@gmail.com>
2023-01-01 16:43:50 +0800

    add upload, upload:uuid api

--- dbTableInit.js
+++ dbTableInit.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const {sequelize} = require('./db/connection')
 const {User} = require('./models/user')
 const {Package} = require('./models/package')
+const {UploadFile} = require('./models/uploadFile')
 
 
 async function syncTableSchema() {
