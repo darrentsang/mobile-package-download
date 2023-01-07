8453f7aed3eb5e9663f6f0c74624077370755a87
Darren Tsang <darren9586@gmail.com>
2023-01-07 15:46:26 +0800

    added api prefix

---
+++ routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+const {authValidate} = require('./services/auth')
+const login = require('./services/auth/login')
+const packages = require('./services/packages')
+const upload = require('./services/upload')
+
+
+router.use('/login', login)
+router.use(authValidate)
+router.use('/packages', packages)
+router.use('/upload', upload)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require('express')
 var cookieParser = require('cookie-parser')
-var cors = require('cors')
 var app = express()
 require('dotenv').config()
 global.__basedir = __dirname;
-
-const {authValidate} = require('./services/auth')
-const login = require('./services/auth/login')
-const packages = require('./services/packages')
-const upload = require('./services/upload')
-
+const routes = require('./routes')
 
 app.use(cookieParser())
 app.use(express.json())
 
-app.use('/login', login)
-app.use(authValidate)
-app.use('/packages', packages)
-app.use('/upload', upload)
+app.use('/api', routes)
 
 
 app.listen(5001, () => { console.log("Server started and listening port 5001...")})
