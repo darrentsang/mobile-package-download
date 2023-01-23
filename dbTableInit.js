ea5fb2b82003639a18a87687463e02ddd2848a55
Darren Tsang <darren9586@gmail.com>
2023-01-23 16:23:36 +0800

    added default uer sql script

--- dbTableInit.js
+++ dbTableInit.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const {sequelize} = require('./utils/db/connection')
-const {User} = require('./models/user')
+const {User, UserRoles} = require('./models/user')
 const {Package} = require('./models/package')
 const {UploadFile} = require('./models/uploadFile')
 
@@ -8,5 +8,40 @@ const {UploadFile} = require('./models/uploadFile')
 async function syncTableSchema() {
     await sequelize.sync();
 }
+async function addDefualtUser() {
+    if((await User.findAll()).length > 0) {
+        return console.log("No default user created")
+    }
 
-syncTableSchema();
+    var user = await User.create({
+        username: "user",
+        password: "123456",
+        roles: [UserRoles.USER]
+    })
+    console.log("Created User with USER access right")
+
+    var admin = await User.create({
+        username: "admin",
+        password: "123456",
+        roles: [UserRoles.ADMIN] 
+    })
+    console.log("Created Admin with ADMIN access right")
+
+    var superUser = await User.create({
+        username: "super",
+        password: "123456",
+        roles: [UserRoles.USER, UserRoles.ADMIN] 
+    })
+    console.log("Created Super with USER and Admin access right")
+}
+
+
+async function init() {
+    await syncTableSchema();
+    await addDefualtUser();
+
+}
+
+init()
+
+
