04f1aa9e1c902cac3188494ec0c6d3311030ceb7
Darren Tsang <darren9586@gmail.com>
2023-03-30 17:14:46 +0800

    code refactor by chatgpt

--- services/packages/index.js
+++ services/packages/index.js
@@ -1,110 +1,109 @@
-const express = require('express')
-const { sequelize } = require('../../utils/db/connection')
-const Package = require('../../models/package')
-const User = require('../../models/user')
-const auth = require('../auth')
-const userConverter = require('../../converter/userConverter')
-const fs = require('fs-extra')
+const express = require('express');
+const fs = require('fs-extra');
+const { Op } = require('sequelize');
+const { sequelize } = require('../../utils/db/connection');
+const Package = require('../../models/package');
+const User = require('../../models/user');
+const auth = require('../auth');
+const userConverter = require('../../converter/userConverter');
 
-const router = express.Router()
-const pageSize = 20
-
+const router = express.Router();
+const pageSize = 20;
 
 router.use((req, res, next) => {
-    const decodeJwt = auth.getJWTClaims(req)
-    const user = userConverter.ConvertSeesionJWTToUser(decodeJwt)
-   if(!checkPermission(user)) return res.sendStatus(403)
-
-    next()
-})
+  const decodedJwt = auth.getJWTClaims(req);
+  const user = userConverter.ConvertSeesionJWTToUser(decodedJwt);
+  if (!checkPermission(user)) return res.sendStatus(403);
+  next();
+});
 
 function checkPermission(user) {
-    if(user.roles && user.roles.includes(User.UserRoles.USER)) return true
-    return false
+  return user.roles && user.roles.includes(User.UserRoles.USER);
 }
 
 router.get('/', async (req, res) => {
-    const offset = (req.query.page || 0) * pageSize
+  const offset = (req.query.page || 0) * pageSize;
 
-    var packages = await Package.findAll({ 
-        order: [['createdAt', 'DESC']],
-        limit: pageSize,
-        offset: offset
-    })
+  const packages = await Package.findAll({
+    order: [['createdAt', 'DESC']],
+    limit: pageSize,
+    offset,
+  });
 
-    res.send(packages)
-})
+  res.send(packages);
+});
 
 router.get('/:id(\\d+)', async (req, res) => {
-
-    var package = await Package.findByPk(req.params.id)
-
-    res.send(package)
-})
+  const package = await Package.findByPk(req.params.id);
+  res.send(package);
+});
 
 router.get('/:id(\\d+).plist', async (req, res) => {
-    var package = await Package.findByPk(req.params.id)
-    fs.readFile(global.__basedir + '/template.plist', function (err, template) {
-      if (err) {
-        throw err; 
-      }
-      const ipaUrl = `https://${process.env.DOMAIN}/api/packages/${package.fileName}?auth=${auth.getJWTFromRequest(req)}`
-      template = template.toString().replace('{{ipa url}}', ipaUrl)
-      template = template.replace('{{app bundle identifier}}', package.bundleIdentifier)
-      template = template.replace('{{bundle version}}', package.versionName)
-      template = template.replace('{{App Title}}', package.displayName)
+  const package = await Package.findByPk(req.params.id);
+  const template = await fs.readFile(
+    `${global.__basedir}/template.plist`,
+    'utf-8',
+  );
+  const ipaUrl = `https://${process.env.DOMAIN}/api/packages/${package.fileName}?auth=${auth.getJWTFromRequest(
+    req,
+  )}`;
+  const modifiedTemplate = template
+    .replace('{{ipa url}}', ipaUrl)
+    .replace('{{app bundle identifier}}', package.bundleIdentifier)
+    .replace('{{bundle version}}', package.versionName)
+    .replace('{{App Title}}', package.displayName);
+  res.set('Content-Type', 'application/xml');
+  res.send(modifiedTemplate);
+});
 
-      res.set('Content-Type', 'application/xml');
-      res.send(template);
-    });
-})
+router.get('/:uuid([a-z0-9\\-]{36}):ext(.ipa|.apk)', async (req, res) => {
+  const { uuid, ext } = req.params;
+  const filePath = `${global.__basedir}/data/packages/${uuid}${ext}`;
+  res.contentType('application/octet-stream');
+  res.setHeader(
+    'Content-Disposition',
+    `attachment: filename="${uuid}${ext}"`,
+  );
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(res);
+});
 
-router.get('/:uuid([a-z0-9\\-]{36}):ext(.ipa|.apk)', async(req, res) => {
-    console.log('get IPA / APK')
-    res.contentType("application/octet-stream");
-    res.setHeader('Content-Disposition', 'attachment: filename="' + req.params.uuid + req.params.ext + '"')
-    const filePath = global.__basedir + `/data/packages/${req.params.uuid}${req.params.ext}`
-    var stream = fs.createReadStream(filePath);
-    stream.pipe(res);
-})
+router.get('/overview', async (req, res) => {
+  const uniquePackageList = await Package.findAll({
+    attributes: [
+      'displayName',
+      [sequelize.fn('MAX', sequelize.col('id')), 'id'],
+    ],
+    group: 'displayName',
+  });
 
-router.get('/overview', async(req, res) => {
-    var uniquePackageList = await Package.findAll({ 
-        attributes: [
-            'displayName',
-            [sequelize.fn("MAX", sequelize.col("id")), "id"]],
-        group: 'displayName'
-    })
+  const packages = await Package.findAll({
+    where: {
+      id: uniquePackageList.map((i) => i.id),
+    },
+    order: [['createdAt', 'DESC']],
+  });
 
-    var packages = await Package.findAll({
-        where: {
-            id: uniquePackageList.map( i => i.id)
-        },
-        order: [['createdAt', 'DESC']]
-    })
+  res.send(packages);
+});
 
+router.post('/versionHistory', async (req, res) => {
+  const { name } = req.body;
+  const versionHistory = await Package.findAll({
+    attributes: [
+      'id',
+      'platform',
+      'versionName',
+      'buildVersion',
+      'displayName',
+      'bundleIdentifier',
+      'fileName',
+    ],
+    where: { displayName: name },
+    order: [['id', 'DESC']],
+  });
 
-    res.send(packages)    
-})
+  res.send(versionHistory);
+});
 
-router.post('/versionHistory', async(req, res) => {
-    console.log(req.body.name)
-    var versionHistory = await Package.findAll({
-        attributes: [
-            'id',
-            'platform',
-            'versionName',
-            'buildVersion',
-            'displayName',
-            'bundleIdentifier',
-            'fileName'],
-        where: { displayName: req.body.name },
-        order: [['id','DESC']]
-    })
-
-    res.send(versionHistory)
-
-})
-
-
-module.exports = router
+module.exports = router;
